2adf46077d11bc537bcbe25e3f13c6160df8e02c
Caio Batista <caiobatista034@gmail.com>
2023-03-24 21:23:47 +0000

    Create TipoPersonagem.java

--- src/Personagem.java
+++ src/Personagem.java
@@ -1,29 +1,31 @@
 package src;
 public class Personagem{
+    private TipoPersonagem tipoPersonagem;
     private int visibilidade;
     private int poder;
     private int vidas;
 
-    private Personagem(int visibilidade, int poder, int vidas){
+    private Personagem(TipoPersonagem tipoPersonagem, int visibilidade, int poder, int vidas){
+        this.tipoPersonagem = tipoPersonagem;
         this.visibilidade = visibilidade;
         this.poder = poder;
         this.vidas = vidas;
     }
 
     public static Personagem criaNormal(){
-        return new Personagem(10, 50, 3);
+        return new Personagem(TipoPersonagem.NORMAL, 10, 50, 3);
     }
 
     public static Personagem criaPoderoso(int poder){
-        return new Personagem(10, poder, 3);
+        return new Personagem(TipoPersonagem.PODEROSO, 10, poder, 3);
     }
 
     public static Personagem criaSoturno(int visibilidade){
-        return new Personagem(visibilidade, 50, 3);
+        return new Personagem(TipoPersonagem.SOTURNO, visibilidade, 50, 3);
     }
 
     public static Personagem criaCustom(int visibilidade, int poder, int vidas){
-        return new Personagem(visibilidade, poder, vidas);
+        return new Personagem(TipoPersonagem.CUSTOM, visibilidade, poder, vidas);
     }
 
     public int getVisibilidade(){
@@ -39,6 +41,6 @@ public class Personagem{
     }
 
     public String toString(){
-        return "Personagem\nvisibildade= " + this.visibilidade + "\npoder= " + this.poder + "\nvidas= " + this.vidas + "\n";
+        return "PERSONAGEM " + tipoPersonagem + "\nvisibildade= " + this.visibilidade + "\npoder= " + this.poder + "\nvidas= " + this.vidas + "\n";
     }
 }
---
+++ src/TipoPersonagem.java
@@ -0,0 +1,4 @@
+package src;
+public enum TipoPersonagem {
+    NORMAL, PODEROSO, SOTURNO, CUSTOM;
+}
